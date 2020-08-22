37a94e254b6562ebea5a1a0e5819edf2a7b4823f
ibrazbib27 <ibrahim.zbib@macys.com>
2020-08-22 16:52:27 -0400

    Update allblogs.ts

--- src/server/routes/allblogs.ts
+++ src/server/routes/allblogs.ts
@@ -1,20 +1,18 @@
-import * as express from 'express';
-import DB from '../db';
+import * as express from "express";
+import DB from "../db";
 let router = express.Router();
 
 //handles get all blogs request
 router.get("/blogs", async (req, res) => {
-    try {
+  try {
+    let blogs;
+    blogs = await DB.blog.GetBlogs();
 
-        let blogs;
-        blogs = await DB.blog.GetBlogs();
-
-        res.json(blogs);
-    }
-    catch(e){
-        console.log(e);
-        res.sendStatus(500);
-    }
+    res.json(blogs);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
 });
 
 export default router;
